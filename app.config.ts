d411b6b8f8a8107faf19f72d150ba6c71fb56fb3
Ciarán Curley <ciaran.curley.11.11@gmail.com>
2025-03-25 14:03:24 +0000

    tailwind v4 migration

--- app.config.ts
+++ app.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from "@tanstack/react-start/config";
 import tsConfigPaths from "vite-tsconfig-paths";
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   server: {
@@ -13,6 +14,11 @@ export default defineConfig({
       tsConfigPaths({
         projects: ["./tsconfig.json"],
       }),
+      // Type mismatch between tailwindcss and vite under vinxi
+      // Tanstack Start plans to remove vinxi as a dependency in the future
+      // so this can be ported to vite where this works
+      // @ts-expect-error
+      tailwindcss(),
     ],
   },
 });
