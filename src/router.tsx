f0121f6da7c52d6c29ce904d6bf245d9a680980b
Ciarán Curley <ciaran.curley.11.11@gmail.com>
2025-03-27 09:34:16 +0000

    fix cors

--- src/router.tsx
+++ src/router.tsx
@@ -56,6 +56,7 @@ export function createRouter() {
       ...request,
       headers: {
         ...request.headers,
+        "Content-Type": "application/json",
         Authorization: "Bearer " + (await tokenPromise),
       },
     };
